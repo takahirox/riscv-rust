2bf0fbedac8afdba6ae34757b73dc262faefba17
Takahiro <hogehoge@gachapin.jp>
2020-03-04 00:58:58 -0800

    Update build_tests.sh to just copy elf files

--- build_tests.sh
+++ build_tests.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 path_to_riscv_tests="../riscv-tests"
-path_to_objcopy="/opt/riscv/bin/riscv64-unknown-elf-objcopy"
 out_directory="$(pwd)/tests"
 
 if [ ! -e $out_directory ]; then
@@ -13,7 +12,7 @@ pushd $path_to_riscv_tests
 for file in $(ls -F ./isa | grep -v / | grep -v Makefile | grep -v .dump | cut -d"*" -f1)
 do
 	echo isa/${file} to ${out_directory}/${file}
-	$path_to_objcopy -O binary ./isa/${file} ${out_directory}/${file}
+	cp isa/${file} ${out_directory}/${file}
 done
 
 popd
