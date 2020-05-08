46c877414565e070d2785a7cac874b2a85a64d67
Takahiro <hogehoge@gachapin.jp>
2020-05-07 18:31:03 -0700

    Move AMOSWAP.D to the new decode system

--- src/cpu.rs
+++ src/cpu.rs
@@ -114,7 +114,6 @@ pub enum TrapType {
 }
 
 enum Instruction {
-	AMOSWAPD,
 	AMOSWAPW,
 	ANDI,
 	AUIPC,
@@ -308,7 +307,6 @@ fn get_trap_cause(trap: &Trap, xlen: &Xlen) -> u64 {
 
 fn get_instruction_name(instruction: &Instruction) -> &'static str {
 	match instruction {
-		Instruction::AMOSWAPD => "AMOSWAP.D",
 		Instruction::AMOSWAPW => "AMOSWAP.W",
 		Instruction::ANDI => "ANDI",
 		Instruction::AUIPC => "AUIPC",
@@ -447,7 +445,6 @@ fn get_instruction_format(instruction: &Instruction) -> InstructionFormat {
 		Instruction::XORI => InstructionFormat::I,
 		Instruction::JAL => InstructionFormat::J,
 		Instruction::FENCE => InstructionFormat::O,
-		Instruction::AMOSWAPD |
 		Instruction::AMOSWAPW |
 		Instruction::DIV |
 		Instruction::DIVU |
@@ -1577,7 +1574,6 @@ impl Cpu {
 				},
 				3 => {
 					match funct7 >> 2 {
-						1 => Instruction::AMOSWAPD,
 						2 => Instruction::LRD,
 						3 => Instruction::SCD,
 						_ => return Err(())
@@ -2070,17 +2066,6 @@ impl Cpu {
 				let rs2 = (word >> 20) & 0x1f; // [24:20]
 				let rs3 = (word >> 27) & 0x1f; //[31:27]
 				match instruction {
-					Instruction::AMOSWAPD => {
-						let tmp = match self.mmu.load_doubleword(self.unsigned_data(self.x[rs1 as usize])) {
-							Ok(data) => data,
-							Err(e) => return Err(e)
-						};
-						match self.mmu.store_doubleword(self.unsigned_data(self.x[rs1 as usize]), self.x[rs2 as usize] as u64) {
-							Ok(()) => {},
-							Err(e) => return Err(e)
-						};
-						self.x[rd as usize] = tmp as i64;
-					},
 					Instruction::AMOSWAPW => {
 						let tmp = match self.mmu.load_word(self.unsigned_data(self.x[rs1 as usize])) {
 							Ok(data) => data,
@@ -2610,7 +2595,7 @@ fn parse_format_r (word: u32) -> FormatR {
 	}
 }
 
-const INSTRUCTION_NUM: usize = 15;
+const INSTRUCTION_NUM: usize = 16;
 
 // @TODO: Reorder in often used order as 
 // @TODO: Move all the instructions to INSTRUCTIONS from the current decode() and operate()
@@ -2800,6 +2785,24 @@ const INSTRUCTIONS: [InstructionData; INSTRUCTION_NUM] = [
 				Err(e) => return Err(e)
 			};
 			match cpu.mmu.store_word(cpu.x[f.rs1] as u64, (cpu.x[f.rs2] | tmp) as u32) {
+				Ok(()) => {},
+				Err(e) => return Err(e)
+			};
+			cpu.x[f.rd] = tmp;
+			Ok(())
+		}
+	},
+	InstructionData {
+		mask: 0xf800707f,
+		data: 0x0800302f,
+		name: "AMOSWAP.D",
+		operation: |cpu, word, _address| {
+			let f = parse_format_r(word);
+			let tmp = match cpu.mmu.load_doubleword(cpu.x[f.rs1] as u64) {
+				Ok(data) => data as i64,
+				Err(e) => return Err(e)
+			};
+			match cpu.mmu.store_doubleword(cpu.x[f.rs1] as u64, cpu.x[f.rs2] as u64) {
 				Ok(()) => {},
 				Err(e) => return Err(e)
 			};
