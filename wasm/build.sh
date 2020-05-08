adf277ddabe94caabfdc322865783ddda7321474
Takahiro <hogehoge@gachapin.jp>
2020-05-07 20:33:24 -0700

    Add workspace to top Cargo.toml

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,3 +1,4 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen ./target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./web --target web --no-typescript
-wasm-bindgen ./target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./nodejs --nodejs 
+wasm-bindgen ../target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./web --target web --no-typescript
+wasm-bindgen ../target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./nodejs --nodejs
+
