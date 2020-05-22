0c610bfce2a72e11f56bb9d8ba05c4f42d959cf9
Takahiro <hogehoge@gachapin.jp>
2020-05-21 19:42:04 -0700

    Update Wasm

--- wasm/nodejs/riscv_emu_rust_wasm.d.ts
+++ wasm/nodejs/riscv_emu_rust_wasm.d.ts
@@ -1,40 +1,105 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
+* `WasmRiscv` is an interface between user JavaScript code and
+* WebAssembly RISC-V emulator. The following code is example
+* JavaScript user code.
+*
+* ```
+* // JavaScript code
+* const riscv = WasmRiscv.new();
+* // Setup program content binary
+* riscv.setup_program(new Uint8Array(elfBuffer));
+* // Setup filesystem content binary
+* riscv.setup_filesystem(new Uint8Array(fsBuffer));
+*
+* // Emulator needs to break program regularly to handle input/output
+* // because the emulator is currenlty designed to run in a single thread.
+* // Once `SharedArrayBuffer` lands by default in major browsers
+* // we would run input/output handler in another thread.
+* const runCycles = () => {
+*   // Run 0x100000 (or certain) cycles, handle input/out,
+*   // and fire next cycles.
+*   // Note: Evety instruction is completed in a cycle.
+*   setTimeout(runCycles, 0);
+*   riscv.run_cycles(0x100000);
+*
+*   // Output handling
+*   while (true) {
+*     const data = riscv.get_output();
+*     if (data !== 0) {
+*       // print data
+*     } else {
+*       break;
+*     }
+*   }
+*
+*   // Input handling. Assuming inputs holds
+*   // input ascii data.
+*   while (inputs.length > 0) {
+*     riscv.put_input(inputs.shift());
+*   }
+* };
+* runCycles();
+* ```
 */
 export class WasmRiscv {
   free(): void;
 /**
+* Creates a new `WasmRiscv`.
 * @returns {WasmRiscv} 
 */
   static new(): WasmRiscv;
 /**
-* @param {Uint8Array} contents 
+* Sets up program run by the program. This method is expected to be called
+* only once.
+*
+* # Arguments
+* * `content` Program binary
+* @param {Uint8Array} content 
 */
-  setup_program(contents: Uint8Array): void;
+  setup_program(content: Uint8Array): void;
 /**
-* @param {Uint8Array} contents 
+* Sets up filesystem. Use this method if program (e.g. Linux) uses
+* filesystem. This method is expected to be called up to only once.
+*
+* # Arguments
+* * `content` File system content binary
+* @param {Uint8Array} content 
 */
-  setup_filesystem(contents: Uint8Array): void;
+  setup_filesystem(content: Uint8Array): void;
 /**
-* @param {Uint8Array} contents 
+* Sets up device tree. The emulator has default device tree configuration.
+* If you want to override it, use this method. This method is expected to
+* to be called up to only once.
+*
+* # Arguments
+* * `content` DTB content binary
+* @param {Uint8Array} content 
 */
-  setup_dtb(contents: Uint8Array): void;
+  setup_dtb(content: Uint8Array): void;
 /**
+* Runs program set by `setup_program()`. The emulator won't stop forever
+* unless [`riscv-tests`](https://github.com/riscv/riscv-tests) programs.
+* The emulator stops if program is `riscv-tests` program and it finishes.
 */
   run(): void;
 /**
+* Runs program set by `setup_program()` in `cycles` cycles.
+*
+* # Arguments
+* * `cycles`
 * @param {number} cycles 
 */
   run_cycles(cycles: number): void;
 /**
 * Runs program until breakpoints. Also known as debugger's continue command.
-* This method takes max_cycles. If the program doesn't hit any breakpoints
-* in max_cycles cycles this method returns false. Otherwise true.
+* This method takes `max_cycles`. If the program doesn't hit any breakpoint
+* in `max_cycles` cycles this method returns `false`. Otherwise `true`.
 *
-* Even without this method, you can write the same behavior JS code as the
-* following code. But JS-WASM bridge cost isn't ignorable now. So this method
-* has been introduced.
+* Even without this method, you can write the same behavior JavaScript code
+* as the following code. But JS-WASM bridge cost isn't ignorable now. So
+* this method has been introduced.
 *
 * ```
 * const runUntilBreakpoints = (riscv, breakpoints, maxCycles) => {
@@ -58,6 +123,8 @@ export class WasmRiscv {
 */
   run_until_breakpoints(breakpoints: BigUint64Array, max_cycles: number): boolean;
 /**
+* Disassembles an instruction Program Counter points to.
+* Use `get_output()` to get the disassembled strings.
 */
   disassemble_next_instruction(): void;
 /**
@@ -80,19 +147,44 @@ export class WasmRiscv {
 */
   load_doubleword(address: BigInt, error: Uint8Array): BigInt;
 /**
+* Reads integer register content.
+*
+* # Arguments
+* * `reg` register number. Must be 0-31.
 * @param {number} reg 
 * @returns {BigInt} 
 */
   read_register(reg: number): BigInt;
 /**
+* Reads Program Counter content.
 * @returns {BigInt} 
 */
   read_pc(): BigInt;
 /**
+* Gets ascii code byte sent from the emulator to terminal.
+* The emulator holds output buffer inside. This method returns zero
+* if the output buffer is empty. So if you want to read all buffered
+* output content, repeatedly call this method until zero is returned.
+*
+* ```
+* // JavaScript code
+* while (true) {
+*   const data = riscv.get_output();
+*   if (data !== 0) {
+*     // print data
+*   } else {
+*     break;
+*   }
+* }
+* ```
 * @returns {number} 
 */
   get_output(): number;
 /**
+* Puts ascii code byte sent from terminal to the emulator.
+*
+* # Arguments
+* * `data` Ascii code byte
 * @param {number} data 
 */
   put_input(data: number): void;
--- wasm/nodejs/riscv_emu_rust_wasm.js
+++ wasm/nodejs/riscv_emu_rust_wasm.js
@@ -55,6 +55,47 @@ function getInt32Memory0() {
     return cachegetInt32Memory0;
 }
 /**
+* `WasmRiscv` is an interface between user JavaScript code and
+* WebAssembly RISC-V emulator. The following code is example
+* JavaScript user code.
+*
+* ```
+* // JavaScript code
+* const riscv = WasmRiscv.new();
+* // Setup program content binary
+* riscv.setup_program(new Uint8Array(elfBuffer));
+* // Setup filesystem content binary
+* riscv.setup_filesystem(new Uint8Array(fsBuffer));
+*
+* // Emulator needs to break program regularly to handle input/output
+* // because the emulator is currenlty designed to run in a single thread.
+* // Once `SharedArrayBuffer` lands by default in major browsers
+* // we would run input/output handler in another thread.
+* const runCycles = () => {
+*   // Run 0x100000 (or certain) cycles, handle input/out,
+*   // and fire next cycles.
+*   // Note: Evety instruction is completed in a cycle.
+*   setTimeout(runCycles, 0);
+*   riscv.run_cycles(0x100000);
+*
+*   // Output handling
+*   while (true) {
+*     const data = riscv.get_output();
+*     if (data !== 0) {
+*       // print data
+*     } else {
+*       break;
+*     }
+*   }
+*
+*   // Input handling. Assuming inputs holds
+*   // input ascii data.
+*   while (inputs.length > 0) {
+*     riscv.put_input(inputs.shift());
+*   }
+* };
+* runCycles();
+* ```
 */
 class WasmRiscv {
 
@@ -72,6 +113,7 @@ class WasmRiscv {
         wasm.__wbg_wasmriscv_free(ptr);
     }
     /**
+    * Creates a new `WasmRiscv`.
     * @returns {WasmRiscv}
     */
     static new() {
@@ -79,35 +121,58 @@ class WasmRiscv {
         return WasmRiscv.__wrap(ret);
     }
     /**
-    * @param {Uint8Array} contents
+    * Sets up program run by the program. This method is expected to be called
+    * only once.
+    *
+    * # Arguments
+    * * `content` Program binary
+    * @param {Uint8Array} content
     */
-    setup_program(contents) {
-        var ptr0 = passArray8ToWasm0(contents, wasm.__wbindgen_malloc);
+    setup_program(content) {
+        var ptr0 = passArray8ToWasm0(content, wasm.__wbindgen_malloc);
         var len0 = WASM_VECTOR_LEN;
         wasm.wasmriscv_setup_program(this.ptr, ptr0, len0);
     }
     /**
-    * @param {Uint8Array} contents
+    * Sets up filesystem. Use this method if program (e.g. Linux) uses
+    * filesystem. This method is expected to be called up to only once.
+    *
+    * # Arguments
+    * * `content` File system content binary
+    * @param {Uint8Array} content
     */
-    setup_filesystem(contents) {
-        var ptr0 = passArray8ToWasm0(contents, wasm.__wbindgen_malloc);
+    setup_filesystem(content) {
+        var ptr0 = passArray8ToWasm0(content, wasm.__wbindgen_malloc);
         var len0 = WASM_VECTOR_LEN;
         wasm.wasmriscv_setup_filesystem(this.ptr, ptr0, len0);
     }
     /**
-    * @param {Uint8Array} contents
+    * Sets up device tree. The emulator has default device tree configuration.
+    * If you want to override it, use this method. This method is expected to
+    * to be called up to only once.
+    *
+    * # Arguments
+    * * `content` DTB content binary
+    * @param {Uint8Array} content
     */
-    setup_dtb(contents) {
-        var ptr0 = passArray8ToWasm0(contents, wasm.__wbindgen_malloc);
+    setup_dtb(content) {
+        var ptr0 = passArray8ToWasm0(content, wasm.__wbindgen_malloc);
         var len0 = WASM_VECTOR_LEN;
         wasm.wasmriscv_setup_dtb(this.ptr, ptr0, len0);
     }
     /**
+    * Runs program set by `setup_program()`. The emulator won't stop forever
+    * unless [`riscv-tests`](https://github.com/riscv/riscv-tests) programs.
+    * The emulator stops if program is `riscv-tests` program and it finishes.
     */
     run() {
         wasm.wasmriscv_run(this.ptr);
     }
     /**
+    * Runs program set by `setup_program()` in `cycles` cycles.
+    *
+    * # Arguments
+    * * `cycles`
     * @param {number} cycles
     */
     run_cycles(cycles) {
@@ -115,12 +180,12 @@ class WasmRiscv {
     }
     /**
     * Runs program until breakpoints. Also known as debugger's continue command.
-    * This method takes max_cycles. If the program doesn't hit any breakpoints
-    * in max_cycles cycles this method returns false. Otherwise true.
+    * This method takes `max_cycles`. If the program doesn't hit any breakpoint
+    * in `max_cycles` cycles this method returns `false`. Otherwise `true`.
     *
-    * Even without this method, you can write the same behavior JS code as the
-    * following code. But JS-WASM bridge cost isn't ignorable now. So this method
-    * has been introduced.
+    * Even without this method, you can write the same behavior JavaScript code
+    * as the following code. But JS-WASM bridge cost isn't ignorable now. So
+    * this method has been introduced.
     *
     * ```
     * const runUntilBreakpoints = (riscv, breakpoints, maxCycles) => {
@@ -149,6 +214,8 @@ class WasmRiscv {
         return ret !== 0;
     }
     /**
+    * Disassembles an instruction Program Counter points to.
+    * Use `get_output()` to get the disassembled strings.
     */
     disassemble_next_instruction() {
         wasm.wasmriscv_disassemble_next_instruction(this.ptr);
@@ -191,6 +258,10 @@ class WasmRiscv {
         }
     }
     /**
+    * Reads integer register content.
+    *
+    * # Arguments
+    * * `reg` register number. Must be 0-31.
     * @param {number} reg
     * @returns {BigInt}
     */
@@ -204,6 +275,7 @@ class WasmRiscv {
         return n0;
     }
     /**
+    * Reads Program Counter content.
     * @returns {BigInt}
     */
     read_pc() {
@@ -216,6 +288,22 @@ class WasmRiscv {
         return n0;
     }
     /**
+    * Gets ascii code byte sent from the emulator to terminal.
+    * The emulator holds output buffer inside. This method returns zero
+    * if the output buffer is empty. So if you want to read all buffered
+    * output content, repeatedly call this method until zero is returned.
+    *
+    * ```
+    * // JavaScript code
+    * while (true) {
+    *   const data = riscv.get_output();
+    *   if (data !== 0) {
+    *     // print data
+    *   } else {
+    *     break;
+    *   }
+    * }
+    * ```
     * @returns {number}
     */
     get_output() {
@@ -223,6 +311,10 @@ class WasmRiscv {
         return ret;
     }
     /**
+    * Puts ascii code byte sent from terminal to the emulator.
+    *
+    * # Arguments
+    * * `data` Ascii code byte
     * @param {number} data
     */
     put_input(data) {
